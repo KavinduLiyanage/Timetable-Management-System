b05523fed3852980d8db4ec5382a229758e4d187
Milinda Ranawaka <milindaranawaka@gmail.com>
2020-08-12 18:18:10 +0530

    Config file

--- TimetableManagementSystem/Students/YearSem.Designer.cs
+++ TimetableManagementSystem/Students/YearSem.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TimetableManagementSystem.Students
 {
-    partial class Students
+    partial class YearSem
     {
         /// <summary>
         /// Required designer variable.
@@ -32,12 +32,12 @@
             // 
             // Students
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(231)))), ((int)(((byte)(239)))));
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(960, 600);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Students";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Padding = new System.Windows.Forms.Padding(15, 49, 15, 16);
             this.Text = "Students";
             this.ResumeLayout(false);
 
--- TimetableManagementSystem/Students/YearSem.cs
+++ TimetableManagementSystem/Students/YearSem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,9 @@ using System.Windows.Forms;
 
 namespace TimetableManagementSystem.Students
 {
-    public partial class Students : Form
+    public partial class YearSem : MetroFramework.Forms.MetroForm
     {
-        public Students()
+        public YearSem()
         {
             InitializeComponent();
         }
