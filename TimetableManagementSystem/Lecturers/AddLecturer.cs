c36729919cb3374783cd7235b686d94dfd2571bf
KavinduLiyanage <sandeepa8956@gmail.com>
2020-09-23 01:31:51 +0530

    Updated header

--- TimetableManagementSystem/Lecturers/AddLecturer.cs
+++ TimetableManagementSystem/Lecturers/AddLecturer.cs
@@ -409,6 +409,25 @@ namespace TimetableManagementSystem.Lecturers
             sessions.ShowDialog();
         }
 
+        private void btnHeaderRooms_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Rooms.Rooms rooms = new Rooms.Rooms();
+            rooms.ShowDialog();
+        }
+
+        private void btnHeaderAdvanced_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdvancedOp.Advanced advc = new AdvancedOp.Advanced();
+            advc.ShowDialog();
+        }
+
+        private void btnHeaderGenerate_Click(object sender, EventArgs e)
+        {
+
+        }
+
         //--------------------Side Nav Buttons--------------------
         private void btnSideNavWorking_Click(object sender, EventArgs e)
         {
@@ -456,6 +475,6 @@ namespace TimetableManagementSystem.Lecturers
             this.Hide();
             Statistics.Statistics stat = new Statistics.Statistics();
             stat.ShowDialog();
-        }
+        }    
     }
 }
--- TimetableManagementSystem/Sessions/Sessions.cs
+++ TimetableManagementSystem/Sessions/Sessions.cs
@@ -81,12 +81,16 @@ namespace TimetableManagementSystem.Sessions
 
         private void btnHeaderRooms_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            Rooms.Rooms rooms = new Rooms.Rooms();
+            rooms.ShowDialog();
         }
 
         private void btnHeaderAdvanced_Click(object sender, EventArgs e)
         {
-
+            this.Hide();
+            AdvancedOp.Advanced advc = new AdvancedOp.Advanced();
+            advc.ShowDialog();
         }
 
         private void btnHeaderGenerate_Click(object sender, EventArgs e)
